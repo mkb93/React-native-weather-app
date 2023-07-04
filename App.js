0ad35888cdae7556b14f44b056695c40fb66221e
marco belahouane <nfk18@hotmail.co.uk>
2023-07-04 18:28:33 +0100

    upcoming weather all

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import React from "react"
+import { View, StyleSheet } from "react-native"
+import CurrentWeather from "./src/components/CurrentWeather"
+import UpcomingWeather from "./src/components/UpcomingWeather"
+const App = () => {
+ return (
+  <View style={styles.container}>
+    <UpcomingWeather />
+  </View>
+ )
 }
-
-const styles = StyleSheet.create({
+const styles = StyleSheet.create ({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+    flex: 1
+  }
+})
+export default App
