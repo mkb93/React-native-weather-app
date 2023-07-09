f2f9e4e5f46771584b9866f2c812924a5cdf32f3
marco belahouane <nfk18@hotmail.co.uk>
2023-07-09 11:32:30 +0100

    connect the weather api

--- App.js
+++ App.js
@@ -1,10 +1,22 @@
-import React from "react"
+import React, { useState, useEffect} from "react"
+import { ActivityIndicator, StyleSheet, View } from "react-native"
 import Tabs from "./src/components/Tabs"
 import { NavigationContainer } from "@react-navigation/native"
-
-
+import * as Location from 'expo-location'
+import {WEATHER_API_KEY } from '@env'
+// api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key}
 
 const App = () => {
+
+  
+  if(loading){
+    return (
+      <View style= {styles.container}>
+        <ActivityIndicator size={'large'} color={'blue'}/>
+      </View>
+    )
+  }
+  
  return (
   <NavigationContainer>
     <Tabs />
@@ -12,5 +24,10 @@ const App = () => {
  )
 }
 
-
+const styles = StyleSheet.create({
+  container: {
+    justifyContent: 'center',
+    flex: 1
+  }
+})
 export default App
